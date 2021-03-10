141100cff74baf3d91e15f9c60870fdec2a728ab
dakugame <39842216+dakugame@users.noreply.github.com>
2021-03-09 22:22:15 -0500

    Update main.go

--- cli/server/main.go
+++ cli/server/main.go
@@ -1,54 +1,18 @@
-package main
+ppackage main
 
 import (
-	"apps/ch.4.2/validator"
-	"html/template"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-const (
-	PORT     = "10000"
-	HOST_URL = "http://localhost:" + PORT
-)
-
-var t *template.Template
-
-type Links struct {
-	BadLinks [][2]string
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+	w.WriteHeader(http.StatusOK)
 }
 
-// invalid links to display for testing.
-var links Links
-
-func index(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, HOST_URL+"/profile", http.StatusTemporaryRedirect)
-}
-func profileHandler(w http.ResponseWriter, r *http.Request) {
-	t.ExecuteTemplate(w, "profile", links)
-}
-func checkProfile(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	p := validator.ProfilePage{&r.Form}
-	t.ExecuteTemplate(w, "submission", p.GetErrors())
-}
-
-// This function is called before main()
-func init() {
-	// Note: we can reference the loaded templates by their defined name inside the template files.
-	t = template.Must(template.ParseFiles("profile.gtpl", "submission.gtpl"))
-
-	list := make([][2]string, 2)
-	list[0] = [2]string{HOST_URL + "/checkprofile", "No data"}
-	list[1] = [2]string{HOST_URL + "/checkprofile?age=1&gender=guy&shirtsize=big", "Invalid options"}
-	links = Links{list}
-}
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/profile", profileHandler)
-	http.HandleFunc("/checkprofile", checkProfile)
-
-	
-		log.Fatal(http.ListenAndServe(":10000", nil))
-	}
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
